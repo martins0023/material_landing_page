cbe4b1e5a8c794e948d987059187aa9c5aa82c27
cluster_wraith <69491293+martins0023@users.noreply.github.com>
2024-05-18 15:52:22 +0100

    Update feature-card.tsx

--- src/components/feature-card.tsx
+++ src/components/feature-card.tsx
@@ -12,15 +12,15 @@ interface FeatureCardProps {
 }
 export function FeatureCard({ icon: Icon, title, children }: FeatureCardProps) {
   return (
-    <Card color="transparent" shadow={false}>
-      <CardBody className="grid place-items-start px-0">
+    <Card color="transparent" shadow={false} {...({} as any)}>
+      <CardBody className="grid place-items-start px-0" {...({} as any)}>
         <div className="mb-3 grid place-content-center rounded-lg text-left text-blue-gray-900">
           <Icon className="h-6 w-6" />
         </div>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
+        <Typography variant="h5" color="blue-gray" className="mb-2" {...({} as any)}>
           {title}
         </Typography>
-        <Typography className="text-gray-500 font-normal">
+        <Typography className="text-gray-500 font-normal" {...({} as any)}>
           {children}
         </Typography>
       </CardBody>
